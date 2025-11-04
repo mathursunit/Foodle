4c63ed34ecc557fb665788ad1fea0b56a0b4739d
mathursunit <92265686+mathursunit@users.noreply.github.com>
2025-11-03 23:22:56 -0500

    V 4.0.11

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // script.js with toast notification and flip animation
-const APP_VERSION = 'v4.0.10';
+const APP_VERSION = 'v4.0.11';
 
 const MS_PER_DAY = 24 * 60 * 60 * 1000;
 const EPOCH_MS = Date.UTC(2025, 0, 1);
